dba1069feb5f52838a116bb1ae40d039881b0b1f
MythriHB <shivuthejas50@gmail.com>
2024-12-10 16:50:14 +0530

    Create area-perimeter_of_triangle.py

---
+++ area-perimeter_of_triangle.py
@@ -0,0 +1,6 @@
+l=float(input("enter the length'"))
+b=float(input("enter the breadth"))
+area=l*b
+perimerter=2*(l+b)
+print("the area of rectangle is :", area)
+print("the perimerter of rectangle is :", perimerter)
