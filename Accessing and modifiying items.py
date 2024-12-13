602169f1dca3c8942197defb94690af183735116
MythriHB <shivuthejas50@gmail.com>
2024-12-13 14:18:03 +0530

    Create Accessing and modifiying items.py

---
+++ Accessing and modifiying items.py
@@ -0,0 +1,16 @@
+#accessing
+print(student["name"])
+
+
+#modifying
+student["age"]=21
+print(student["age"])
+
+#Adding
+student["grade"]="A"
+print(student["grade"])
+
+
+#removing
+del student["age"]
+student
