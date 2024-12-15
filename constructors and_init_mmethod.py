ac524ec73db1da8733430f865a5dbc2244d55462
MythriHB <shivuthejas50@gmail.com>
2024-12-15 13:58:44 +0530

    Create constructors and_init_mmethod.py

---
+++ constructors and_init_mmethod.py
@@ -0,0 +1,61 @@
+class car:
+  no_of_wheels =0
+  milege =70
+class car:
+  no_of_wheels =0
+  milege =70
+  no_of_seats =4
+  def moveforward(self):
+   print("moving forward")
+  def movebacward(self):
+    print("moving bacward")
+  def turnleft(self):
+    print("turning left")
+  def turnright(self):
+    print("turning right")
+
+car1=car()
+print(car1.no_of_seats)
+print(car1.milege)
+print(car1.no_of_wheels)
+
+car3=car()
+car3.milege=100
+car3.no_of_wheels=10
+car.no_of_seats=12
+print(car3.no_of_seats)
+print(car3.milege)
+print(car3.no_of_wheels)
+car3.moveforward()
+car3.movebacward()
+car3.turnleft()
+car3.turnright()
+def turnleft(self):
+    print("turning left")
+def turnright(self):
+    print("turning right")
+
+car1=car()
+print(car1.no_of_seats)
+print(car1.milege)
+print(car1.no_of_wheels)
+
+
+car2 = car()
+print(car2.no_of_seats)
+print(car2.milege)
+print(car2.no_of_wheels)
+
+car3=car()
+car3.milege=100
+car3.no_of_wheels=10
+car.no_of_seats=12
+print(car3.no_of_seats)
+print(car3.milege)
+print(car3.no_of_wheels)
+
+
+car3.moveforward()
+car3.movebacward()
+car3.turnleft()
+car3.turnright()
